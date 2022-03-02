f5d6124b31b35dc8913e0beec514cdf7d6549277
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-03-03 01:52:52 +0200

    worked on the duration and current time features

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
@@ -104,10 +106,32 @@ loadSong(songs[songIndex]);
 function updateProgressBar(e) {
     if(isPlaying) {
         const { duration, currentTime } = e.srcElement;
-        // console.log(duration, currentTime);
         // Update progress bar width 
         const progressPercent = (currentTime / duration) * 100;
-        progress.style.width = `${progressPercent}%`
+        progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        console.log('minutes', durationMinutes);
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        console.log('seconds', durationSeconds);
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        // Delay switching duration Element to avoid NaN
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+        // Calculate display for duration
+        const currentMinutes = Math.floor(currentTime / 60);
+        console.log('minutes', currentMinutes);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        console.log('seconds', durationSeconds);
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
 
